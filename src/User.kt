eae39397b71d84009e26057af8186bbeaf071249
jitendra-classpass <jitendra.pandey@classpass.com>
2022-08-04 03:51:08 -0700

    multiple constructors in kotlin.

--- src/User.kt
+++ src/User.kt
@@ -1,6 +1,14 @@
 package src
 
-class User constructor(var firstName: String, var lastName: String){ // primary constructor
+class User constructor(var firstName: String, var lastName: String) { // primary constructor
+
+    constructor(firstName: String) : this(firstName, "Pakulski") {
+        println("Constructor with single argument is called.")
+    }
+
+    constructor() : this("Steven", "Smith") {
+        println("Constructor with zero argument called.")
+    }
 
     fun printFullName() {
         println("Full Name: $firstName $lastName")
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,18 @@
 package src
 
-fun main(){
+fun main() {
     println("Hello Kotlin")
 
     var Donn = User("Donn", "Felker")
     Donn.printFullName()
+
+    var jane = User()
+    jane.firstName = "Jane"; jane.lastName = "Doe" // this will override the default arguments.
+    jane.printFullName()
+
+    var stevenSmith = User()
+    stevenSmith.printFullName()
+
+    var bob = User("Bob")
+    bob.printFullName()
 }
